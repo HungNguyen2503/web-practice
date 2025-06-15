14a9d6a8304ba97f2bfcde39a9befbad1ebd292f
NguyenPhiHung <hungmun42@gmail.com>
2025-06-15 10:24:19 +0700

    create toast notificaiton

---
+++ day11-toast-notification/main.js
@@ -0,0 +1,47 @@
+let control = document.querySelectorAll(".control li");
+let htmlToasts = document.querySelector(".toasts");
+
+const toasts = {
+	success: {
+		icon: '<i class="fas fa-check-circle"></i>',
+		msg: 'This is a success message !',
+        bg: '#008000'
+    },
+	error: {
+		icon: '<i class="fas fa-exclamation-triangle"></i>',
+		msg: 'This is a error message !',
+        bg: '#ff0000'
+    },
+	warning: {
+		icon: '<i class="fas fa-exclamation-circle"></i>',
+		msg: 'This is a warning message !',
+        bg:'#d2d200'
+	},
+}
+
+control.forEach(button => {
+    button.addEventListener('click', e =>{
+        let toast = document.createElement("div");
+        let msg = document.createElement("p");
+        let countdown = document.createElement("span")
+        
+        msg.innerHTML = `${toasts[e.target.className].msg}`;
+        countdown.style.backgroundColor = `${toasts[e.target.className].bg}`;
+        
+        
+        toast.classList.add('toast');
+        toast.innerHTML = `${toasts[e.target.className].icon}`;
+        toast.appendChild(msg);
+        toast.appendChild(countdown);
+
+        toast.style.backgroundColor = `${toasts[e.target.className].bg}66`;
+        toast.style.borderColor = `${toasts[e.target.className].bg}`;
+
+        htmlToasts.appendChild(toast);
+
+        setTimeout(()=>{
+            toast.remove();
+        }, 5000);
+
+    });
+});
